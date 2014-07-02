2e3a8c71e49d424777dca9bf9a77e9f807b67ad4
Jon Poler <jdp5087@gmail.com>
2014-07-02 15:56:26 -0400

    working on deque exercise

--- mit/6-001/sicp/ch3/deque.scm
+++ mit/6-001/sicp/ch3/deque.scm
@@ -4,8 +4,10 @@
   (cdr (car ptr)))
 (define (forward ptr)
   (cdr ptr))
-(define (create-link e f b)
-  (cons (cons
+(define (make-link-front ele after)
+  (cons (cons ele '()) after))
+(define (make-link-back ele before)
+  (cons (cons ele before) '()))
 
 (define (front-ptr deque)
   (car deque))
@@ -14,19 +16,65 @@
 (define (empty-deque? deque)
   (or (null? (front-ptr deque))
       (null? (rear-ptr deque))))
-
+(define (make-deque)
+  (cons '() '()))
   
 (define (front-deque deque)
   (if (empty-deque? deque)
       (error "called front deque on an empty deque " deque)
-      (car (front-ptr deque)))
+      (element (front-ptr deque))))
+(define (rear-deque deque)
+  (if (empty-deque? deque)
+      (error "called rear-deque on an empty deque " deque)
+      (element (rear-ptr deque))))
 
-(define (make-deque)
-  (cons '() '()))
-(define (set-front-deque! deque item)
-  (
+(define (front-insert-deque! deque value)
+  (if (empty-deque? deque)
+      (let ((new (make-link-front value '())))
+	(set-car! deque new)
+	(set-cdr! deque new)
+	value)
+      (begin
+	(set-car! deque (make-link-front value (front-ptr deque)))
+	value)))
+
+(define (back-insert-deque! deque value)
+  (if (empty-deque? deque)
+      (let ((new (make-link-front value '())))
+	(set-car! deque new)
+	(set-cdr! deque new))
+      (begin
+	(set-cdr! deque (make-link-back value (rear-ptr deque)))
+	value)))
+
+(define (front-delete-deque! deque)
+  (if (empty-deque? deque)
+      (error "front-delete-deque called on an empty deque " deque)
+      (set-car! deque (forward (front-ptr deque)))))
+
+(define (back-delete-deque! deque)
+  (if (empty-deque? deque)
+      (error "back-delete-deque called on an empty deque " deque)
+      (set-cdr! deque (backward (rear-ptr deque)))))
+
+		
+(define d (make-deque))
+
+(front-insert-deque! d '1)
+(front-insert-deque! d '2)
+(front-insert-deque! d '3)
+(back-insert-deque! d '0)
+
+(front-deque d)
+(rear-deque d)
+(front-delete-deque! d)
+(back-delete-deque! d)
 
 
 
 
+;;; change so that all references to back change to rear
 
+;;; after adding second element, backward of rear-ptr should point to next element
+
+;; or consider just making a circular reference on null, however this seems like it could complicate things.
